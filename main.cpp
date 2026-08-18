e77f73844737af04847ceed3fb789bd8869892d1
kacper <kacper.kielbasa5000@gmail.com>
2026-08-19 01:39:45 +0200

    rewrite for readability

--- main.cpp
+++ main.cpp
@@ -15,93 +15,76 @@
 
 class CPU {
     public:
-        void step();
-        void MOV(std::size_t register_number, int value);
-        void MOV(std::size_t register_number1, std::size_t register_number2);
-        uint8_t fetch();
-    
-
-
-    uint16_t registers[8];
-    
+    uint8_t registers[8] {};
     uint16_t pc {};
-    uint16_t sp {4095};
-    
-    uint8_t memory[4096];
+    uint16_t sp {};
+    uint8_t flags {};
 
-    bool halted = {};
-};
+    uint8_t memory[4096] {};
 
-enum class Instruction: uint8_t {
-    MOV = 0x01, 
-    ADD = 0x02, 
-    SUB = 0x03,
-    PUSH = 0x04,
-    HLT  = 0x05,
-};
+    bool halt {};
 
-void CPU::MOV(std::size_t register_number, int value){
-    registers[register_number] = value;
-}
+    ////
 
-
-void CPU::MOV(std::size_t register_number_dst, std::size_t register_number_src){
-    registers[register_number_dst] = registers[register_number_src];
-}
-
-uint8_t CPU::fetch() {
-    return memory[pc++];
-}
-
-void CPU::step(){
-    Instruction optcode = static_cast<Instruction>(fetch());
-
-    switch (optcode){
-        case Instruction::MOV: {
-            auto reg = fetch();
-            auto val = fetch();
-            MOV(reg, val);
-            break;
-        }
-
-        case Instruction::ADD: 
-            // ...
-            break;
-
-        case Instruction::SUB: 
-            // ...
-            break;
-
-        case Instruction::PUSH:
-            //
-            break;
-
-        case Instruction::HLT:
-            this->halted = 1;
-            break;
+    uint8_t fetch(){
+        return memory[pc++];
     }
-}
+
+    void step(){
+        uint8_t opcode = fetch();
+
+        switch(opcode) {
+            case 0x01: {
+                uint8_t first = fetch();
+                uint8_t second = fetch();
+                MOV(first, second);
+                break;
+            }
+            
+            case 0xFF: {
+                HALT();
+                break;
+            }
+        }
+    }
+
+    void HALT() { this->halt = true; }
+    void MOV(std::size_t register_number, int value) { registers[register_number] = value; }
+    void MOV(std::size_t register_number_dst, std::size_t register_number_src) { registers[register_number_dst] = registers[register_number_src]; }
+};
+
+
+enum ISA {
+    MOV   = 0x01,
+    ADD   = 0x02,
+    SUB   = 0x03,
+    LOAD  = 0x04,
+    STORE = 0x05,
+    JMP   = 0x06,
+    CMP   = 0x07,
+    JZ    = 0x08,
+    HALT  = 0xFF
+};
+
 
 int main(void){
+    /*MOV r0, 10
+    MOV r1, 20
+    ADD r0, r1
+    HALT */
     CPU cpu;
 
-    cpu.memory[0] = static_cast<uint8_t>(Instruction::MOV);
-    cpu.memory[1] = 0;      // R0
-    cpu.memory[2] = 10;     // wartość
+    cpu.memory[0] = MOV;
+    cpu.memory[1] = 0;
+    cpu.memory[2] = 20;
+    cpu.memory[3] = HALT;
 
-    cpu.memory[3] = static_cast<uint8_t>(Instruction::MOV);
-    cpu.memory[4] = 1;      // R1
-    cpu.memory[5] = 20;
-
-    cpu.memory[6] = static_cast<uint8_t>(Instruction::ADD);
-    cpu.memory[7] = 0;      // R0
-    cpu.memory[8] = 1;      // R1
-
-    cpu.memory[9] = static_cast<uint8_t>(Instruction::HLT);
-
-    while(!cpu.halted){
+    while(!cpu.halt){
         cpu.step();
+        std::cout << "active ";
     }
+
+    std::cout << "\n";
 
     return 0;
 }
